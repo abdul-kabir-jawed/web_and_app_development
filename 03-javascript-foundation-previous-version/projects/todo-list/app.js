b116ef78a6a72ebdd5f49a6d495e1096a17b4dc7
Abdul Kabir <abdulkabirlive1@gmail.com>
2025-07-18 02:42:17 +0500

    Update app.js

--- 03-javascript-foundation-previous-version/projects/todo-list/app.js
+++ 03-javascript-foundation-previous-version/projects/todo-list/app.js
@@ -1,37 +1,38 @@
 var task=document.getElementById("field").value
-
+/*Flags*/
 var doneCount = 0;
 var cancelCount = 0;
 var count=0
 
-array=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','s','r','t','u','v','w','x','y','z']
-
+/*Task Creation*/
 function addTask(task){
-    var hasLetter = array.some(letter => task.toLowerCase().includes(letter));
-    if (hasLetter){
+    if (!(task.trim()=="")){
+    count+=1
+    task_num=count
     var taskCode= `
-    <div class='user_task'>
-      <h2 class='task_index'>Task ${count+1}:</h2>
-      <p>${task}</p>
-      <div id='choose'>
-        <div id='done' onclick='done(this)'><i class='fa-solid fa-check'></i></div>
-        <div id='cancel' onclick='cancel(this)'><i class='fa-solid fa-x'></i></div>
-      </div>
+  <div class='user_task' data-tasknum="${count}">
+    <h2 class='task_index'>Task ${count}:</h2>
+    <p class="task_p">${task}</p>
+    <div id='choose'>
+      <button type="button" class="custom_button" onclick="edit_appear(this)"><i class="fa-regular fa-pen-to-square"></i></button>
+      <div id='done' onclick='done(this)'><i class='fa-solid fa-check'></i></div>
+      <div id='cancel' onclick='cancel(this)'><i class='fa-solid fa-x'></i></div>
     </div>
-  `;
+  </div>
+`;
     var taskCreate=document.getElementById("tasks")
     taskCreate.innerHTML+=taskCode
     var empty= document.getElementById("field")
     empty.value=""
-    count+=1
+    
     var counterChange=document.getElementById("task_num")
     var countStr="Current Task: "
     counterChange.innerHTML=countStr + count
-    return;
+    return task,count;
     }
     
 }
-
+/*Task Done*/
 function done(element){
     var taskDiv = element.closest(".user_task");
     taskDiv.remove();
@@ -46,7 +47,7 @@ function done(element){
     counterChange.innerHTML=countStr + count
 
 }
-
+/*Task Cancel*/
 function cancel(element){
     var taskDiv = element.closest(".user_task");
     taskDiv.remove();
@@ -60,4 +61,47 @@ function cancel(element){
     var countStr="Current Task: "
     counterChange.innerHTML=countStr + count
 
+}
+/*Task Edit*/
+function edit_appear(button) {
+  var taskDiv = button.closest('.user_task');
+  var task_num = taskDiv.getAttribute('data-tasknum');
+  var taskText = taskDiv.querySelector('.task_p').innerText;
+  taskDiv.outerHTML = `<div id="edit_bar" data-tasknum="${task_num}" data-tasktext="${encodeURIComponent(taskText)}">
+    <input id="edit_box" type="text" value="${taskText}"></input>
+    <div id="edit_button">
+      <div id='done_edit' onclick="done_replace(this)"><i class='fa-solid fa-check'></i></div>
+      <div id='cancel_edit' onclick="cancel_replace(this)"><i class='fa-solid fa-x'></i></div>
+    </div>
+  </div>`;
+}
+/*Task Edit Done*/
+function done_replace(element){
+  var replace_html = element.closest('#edit_bar');
+  var value = replace_html.querySelector('#edit_box').value;
+  var task_num = replace_html.getAttribute('data-tasknum');
+  replace_html.outerHTML = `<div class='user_task' data-tasknum="${task_num}">
+      <h2 class='task_index'>Task ${task_num}:</h2>
+      <p class="task_p">${value}</p>
+      <div id='choose'>
+        <button type="button" class="custom_button" onclick="edit_appear(this)"><i class="fa-regular fa-pen-to-square"></i></button>
+        <div id='done' onclick='done(this)'><i class='fa-solid fa-check'></i></div>
+        <div id='cancel' onclick='cancel(this)'><i class='fa-solid fa-x'></i></div>
+      </div>
+    </div>`;
+}
+/*Task Edit Cancel*/
+function cancel_replace(element){
+  var replace_html = element.closest('#edit_bar');
+  var task_num = replace_html.getAttribute('data-tasknum');
+  var text = decodeURIComponent(replace_html.getAttribute('data-tasktext'));
+  replace_html.outerHTML = `<div class='user_task' data-tasknum="${task_num}">
+      <h2 class='task_index'>Task ${task_num}:</h2>
+      <p class="task_p">${text}</p>
+      <div id='choose'>
+        <button type="button" class="custom_button" onclick="edit_appear(this)"><i class="fa-regular fa-pen-to-square"></i></button>
+        <div id='done' onclick='done(this)'><i class='fa-solid fa-check'></i></div>
+        <div id='cancel' onclick='cancel(this)'><i class='fa-solid fa-x'></i></div>
+      </div>
+    </div>`;
 }
