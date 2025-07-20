cb7ef31c94ba39fa2e5f00bfb86d3788604475e3
Abdul Kabir <abdulkabirlive1@gmail.com>
2025-07-20 14:48:34 +0500

    Update app.js

--- 03-javascript-foundation-previous-version/projects/todo-list/app.js
+++ 03-javascript-foundation-previous-version/projects/todo-list/app.js
@@ -1,15 +1,27 @@
-var task=document.getElementById("field").value
 /*Flags*/
 var doneCount = 0;
 var cancelCount = 0;
-var count=0
+var count = 0
+
+/*storing value*/
+todo_array = []
+
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    addTask();
+  }
+});
+
 
 /*Task Creation*/
-function addTask(task){
-    if (!(task.trim()=="")){
-    count+=1
-    task_num=count
-    var taskCode= `
+function addTask() {
+  var task = document.getElementById("field").value
+  if (!(task.trim() == "")) {
+    count += 1
+    task_num = count
+    /*Editing todo div html*/
+    var taskCode = `
   <div class='user_task' data-tasknum="${count}">
     <h2 class='task_index'>Task ${count}:</h2>
     <p class="task_p">${task}</p>
@@ -20,47 +32,77 @@ function addTask(task){
     </div>
   </div>
 `;
-    var taskCreate=document.getElementById("tasks")
-    taskCreate.innerHTML+=taskCode
-    var empty= document.getElementById("field")
-    empty.value=""
-    
-    var counterChange=document.getElementById("task_num")
-    var countStr="Current Task: "
-    counterChange.innerHTML=countStr + count
-    return task,count;
-    }
-    
+    var taskCreate = document.getElementById("tasks")
+    taskCreate.innerHTML += taskCode
+
+    /*Emptying field div*/
+    var empty = document.getElementById("field")
+    empty.value = ""
+
+    /*storing value*/
+
+    todo_array.push({ title: task, id: count });
+
+    /*Updating count*/
+    var counterChange = document.getElementById("task_num")
+    var countStr = "Current Task: "
+    counterChange.innerHTML = countStr + count
+
+
+    return task, count;
+  }
+
 }
 /*Task Done*/
-function done(element){
-    var taskDiv = element.closest(".user_task");
-    taskDiv.remove();
-    doneCount+=1
-    var counterChange=document.getElementById("task_done")
-    var countStr="Done Task: "
-    counterChange.innerHTML=countStr + doneCount
+function done(element) {
+  var taskDiv = element.closest(".user_task");
+  var task_num = parseInt(taskDiv.getAttribute('data-tasknum'));
+  // Remove from array
+  for (var i = 0; i < todo_array.length; i++) {
+    if (todo_array[i].id === task_num) {
+      todo_array.splice(i, 1);
+      break;
+    }
+  }
 
-    count-=1
-    var counterChange=document.getElementById("task_num")
-    var countStr="Current Task: "
-    counterChange.innerHTML=countStr + count
 
+  taskDiv.remove();
+  doneCount += 1
+  var counterChange = document.getElementById("task_done")
+  var countStr = "Done Task: "
+  counterChange.innerHTML = countStr + doneCount
+
+
+  count -= 1
+  var counterChange = document.getElementById("task_num")
+  var countStr = "Current Task: "
+  counterChange.innerHTML = countStr + count
+  reindexTasks();
 }
 /*Task Cancel*/
-function cancel(element){
-    var taskDiv = element.closest(".user_task");
-    taskDiv.remove();
-    cancelCount+=1
-    var counterChange=document.getElementById("task_cancel")
-    var countStr="Cancel Task: "
-    counterChange.innerHTML=countStr + cancelCount
+function cancel(element) {
+  var taskDiv = element.closest(".user_task");
+  var task_num = parseInt(taskDiv.getAttribute('data-tasknum'));
+  // Remove from array
+  for (var i = 0; i < todo_array.length; i++) {
+    if (todo_array[i].id === task_num) {
+      todo_array.splice(i, 1);
+      break;
+    }
+  }
 
-    count-=1
-    var counterChange=document.getElementById("task_num")
-    var countStr="Current Task: "
-    counterChange.innerHTML=countStr + count
+  taskDiv.remove();
+  cancelCount += 1
+  var counterChange = document.getElementById("task_cancel")
+  var countStr = "Cancel Task: "
+  counterChange.innerHTML = countStr + cancelCount
 
+
+  count -= 1
+  var counterChange = document.getElementById("task_num")
+  var countStr = "Current Task: "
+  counterChange.innerHTML = countStr + count
+  reindexTasks();
 }
 /*Task Edit*/
 function edit_appear(button) {
@@ -76,7 +118,7 @@ function edit_appear(button) {
   </div>`;
 }
 /*Task Edit Done*/
-function done_replace(element){
+function done_replace(element) {
   var replace_html = element.closest('#edit_bar');
   var value = replace_html.querySelector('#edit_box').value;
   var task_num = replace_html.getAttribute('data-tasknum');
@@ -89,11 +131,17 @@ function done_replace(element){
         <div id='cancel' onclick='cancel(this)'><i class='fa-solid fa-x'></i></div>
       </div>
     </div>`;
+  for (var i = 0; i < todo_array.length; i++) {
+    if (todo_array[i].id === task_num) {
+      todo_array[i].title = value;
+      break;
+    }
+  }
 }
 /*Task Edit Cancel*/
-function cancel_replace(element){
+function cancel_replace(element) {
   var replace_html = element.closest('#edit_bar');
-  var task_num = replace_html.getAttribute('data-tasknum');
+  var task_num = parseInt(replace_html.getAttribute('data-tasknum'));
   var text = decodeURIComponent(replace_html.getAttribute('data-tasktext'));
   replace_html.outerHTML = `<div class='user_task' data-tasknum="${task_num}">
       <h2 class='task_index'>Task ${task_num}:</h2>
@@ -104,4 +152,34 @@ function cancel_replace(element){
         <div id='cancel' onclick='cancel(this)'><i class='fa-solid fa-x'></i></div>
       </div>
     </div>`;
+  console.log(todo_array);
+}
+
+function reindexTasks(todo_array) {
+  var tasks = document.querySelectorAll('.user_task');
+
+
+  for (var i = 0; i < tasks.length; i++) {
+    var newNum = i + 1;
+    tasks[i].setAttribute('data-tasknum', newNum);
+    var h2 = tasks[i].querySelector('.task_index');
+    if (h2) {
+      h2.innerText = `Task ${newNum}:`;
+    }
+  }
+
+  var newTodoArray = [];
+  for (var i = 0; i < tasks.length; i++) {
+    var taskElement = tasks[i];
+    var taskText = taskElement.querySelector('.task_p').innerText;
+    var newId = i + 1;
+
+    newTodoArray.push({
+      title: taskText,
+      id: newId
+    });
+
+  }
+  todo_array = newTodoArray
+  console.log('Reindexed todo_array:', todo_array);
 }
